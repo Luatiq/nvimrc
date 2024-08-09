cb6451cd0d33913f4725277f99f4a6d90aa40f93
Lua van Loon <git@luatess.email>
2024-08-09 13:51:39 +0200

    moved plugins without/w minimal config to init.lua

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,3 +1,20 @@
 return {
-    'tpope/vim-fugitive'
+    'tpope/vim-fugitive',
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+    },
+    {
+        'mbbill/undotree',
+        keys = {
+            { '<leader>u', '<cmd>UndotreeToggle<cr>', desc = 'Toggle undotree' },
+        }
+    },
 }
